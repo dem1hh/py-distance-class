6713cb89fb718c24af18b303190b4d573a7e8997
Dima Sukhariev <dmitriu.sy1303@gmail.com>
2026-04-14 13:01:49 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,77 @@
 class Distance:
     # Write your code here
-    pass
+    def __init__(self, km: int):
+        self.km = km
+    def __str__(self):
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self):
+        return f"Distance(km={self.km})"
+
+    def __add__(self, otherKm):
+        if isinstance(otherKm, Distance):
+            return Distance(self.km + otherKm.km)
+        if isinstance(otherKm, (int, float)):
+            return Distance(self.km + otherKm)
+        return NotImplemented
+
+    def __iadd__(self, otherKm):
+        if isinstance(otherKm, Distance):
+            self.km += otherKm.km
+            return self
+        if isinstance(otherKm, (int, float)):
+            self.km += otherKm
+            return self
+        return NotImplemented
+
+    def __mul__(self, otherKm):
+        if isinstance(otherKm, (int, float)):
+            return Distance(self.km * otherKm)
+        return NotImplemented
+
+    def __rmul__(self, otherKm):
+        return self.mul(otherKm)
+
+    def __truediv__(self, otherKm):
+        if isinstance(otherKm, (int, float)):
+            result = round(self.km / otherKm, 2)
+            return Distance(result)
+        return NotImplemented
+
+    def __lt__(self, otherKm):
+        if isinstance(otherKm, Distance):
+            return self.km < otherKm
+
+        if isinstance(otherKm, (int, float)):
+            return self.km < otherKm
+        return NotImplemented
+
+    def __gt__(self, otherKm):
+        if isinstance(otherKm, Distance):
+            return self.km > otherKm
+
+        if isinstance(otherKm, (int, float)):
+            return self.km > otherKm
+        return NotImplemented
+
+    def __eq__(self, otherKm):
+        if isinstance(otherKm, Distance):
+            return self.km == otherKm
+
+        if isinstance(otherKm, (int, float)):
+            return self.km == otherKm
+        return NotImplemented
+
+    def __le__(self, otherKm):
+        if isinstance(otherKm, Distance):
+            return self.km <= otherKm
+        if isinstance(otherKm, (int, float)):
+            return self.km <= otherKm
+        return NotImplemented
+
+    def __ge__(self, otherKm):
+        if isinstance(otherKm, Distance):
+            return self.km >= otherKm
+        if isinstance(otherKm, (int, float)):
+            return self.km >= otherKm
+        return NotImplemented
